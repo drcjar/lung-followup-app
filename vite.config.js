81583ef8955bd00b98967e8ce569f1ef8e9aa6fb
drcjar <drcjar@gmail.com>
2025-04-14 23:10:09 +0100

    Final styling + deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/lung-followup-app/', // <- matches your repo name
   plugins: [react()],
-  base: '/lung-followup-app/'
-})
+});
+
